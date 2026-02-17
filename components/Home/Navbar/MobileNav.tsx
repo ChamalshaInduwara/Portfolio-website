81a4ac8da892edf5efd2746d7b158441198bd58d
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-02-17 23:21:29 +0530

    Add fullscreen overlay to MobileNav
    Replace the placeholder text with a fixed, full-screen overlay div for the mobile navigation. The new element uses Tailwind classes (fixed inset-0, transform, transition-all, right-0, duration-500, z-[100002], bg-black, opacity-70, w-full, h-screen) to render a semi-transparent backdrop. Also normalize the default export statement (added semicolon).

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <div>
+      {/* overlay */}
+      <div className="fixed inset-0 transform transition-all right-0 duration-500 z-[100002] bg-black opacity-70 w-full h-screen"></div>
+    </div>
   )
 }
 
-export default MobileNav
+export default MobileNav;
