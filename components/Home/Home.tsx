ce12215d490783a723f44c04eee5098d15b0aa92
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-02-25 22:43:07 +0530

    Add Services section and ServiceCard component
    Add a new Services feature and integrate it into the Home page. Created components/Home/Services/Services.tsx (responsive section with heading and grid) and components/Home/Services/ServiceCard.tsx (placeholder card), and updated components/Home/Home.tsx to import and render <Services />. ServiceCard is currently a simple placeholder to be implemented further.

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "./Hero/Hero";
+import Services from "./Services/Services";
 
 const Home = () => {
   return <div className="overflow-hidden h-[2000px]">\
     <Hero />
+    <Services />
   </div>;
 }
 
