bdfc264bfb915085d9757cb5ea706158e330f54c
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-03-02 00:25:24 +0530

    Add Resume and Projects sections to Home
    Introduce Resume and Projects components and include them in Home. Created Resume (with ResumeCard and icon support) and Projects placeholder components, and updated Home.tsx to import and render them. Also removed the hardcoded container height from Home to allow the new sections to expand naturally; styling uses existing Tailwind classes.

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Hero from "./Hero/Hero";
 import Services from "./Services/Services";
+import Resume from "./Resume/Resume";
+import Projects from "./Projects/Projects";
 
 const Home = () => {
-  return <div className="overflow-hidden h-[2000px]">\
+  return <div className="overflow-hidden">
     <Hero />
     <Services />
+    <Resume />
+    <Projects />
   </div>;
 }
 
