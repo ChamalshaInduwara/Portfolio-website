ab3c78ce1d0acb439083497e972a8d7052ca4852
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-08-16 23:54:59 +0530

    Refactor Home and Skills components for improved readability and structure

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -8,34 +8,32 @@ import Skills from "./Skills/Skills";
 import ClientReview from "./ClientReview/ClientReview";
 import Blog from "./Blog/Blog";
 import Contact from "./Contact/Contact";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import {useEffect} from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Home = () => {
-
   useEffect(() => {
-    const initAOS = async () => {
-      await import('aos');
-      AOS.init({
-        duration: 1000,
-        easing: "ease",
-        once: true,
-        anchorPlacement: "top-bottom",
-      });
-    };
-    initAOS();
-  },[])
-  return <div className="overflow-hidden">
-    <Hero />
-    <Services />
-    <Resume />
-    <Projects />
-    <Skills />
-    <ClientReview />
-    <Blog />
-    <Contact />
-  </div>;
-}
+    AOS.init({
+      duration: 1000,
+      easing: "ease",
+      once: true,
+      anchorPlacement: "top-bottom",
+    });
+  }, []);
+
+  return (
+    <div className="overflow-hidden">
+      <Hero />
+      <Services />
+      <Resume />
+      <Projects />
+      <Skills />
+      <ClientReview />
+      <Blog />
+      <Contact />
+    </div>
+  );
+};
 
 export default Home;
--- components/Home/Skills/Skills.tsx
+++ components/Home/Skills/Skills.tsx
@@ -1,62 +1,87 @@
 "use client";
-import React from 'react';
-import { FaPercentage } from 'react-icons/fa';
-import { SiJavascript, SiNextdotjs, SiNodedotjs, SiReact, SiTailwindcss, SiTypescript } from 'react-icons/si';
+import React from "react";
+import {
+  SiDotnet,
+  SiJavascript,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiReact,
+  SiSharp,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
 
-import Tilt from 'react-parallax-tilt';
+import Tilt from "react-parallax-tilt";
 
-const skills =[
-    {
-        name: "JavaScript",
-        icon:<SiJavascript />,
-        percentage: 89,
-    },
-    {
-        name: "React Js",
-        icon:<SiReact />,
-        percentage: 92,
-    },
-    {
-        name: "Next Js",
-        icon:<SiNextdotjs />,
-        percentage: 90,
-    },
-    {
-        name: "Node Js",
-        icon:<SiNodedotjs />,
-        percentage: 88,
-    },
-    {
-        name: "Tailwind CSS",
-        icon:<SiTailwindcss />,
-        percentage: 94,
-    },
-    {
-        name: "Typescript",
-        icon:<SiTypescript />,
-        percentage: 86,
-    }
+const skills = [
+  {
+    name: "JavaScript",
+    icon: <SiJavascript />,
+    percentage: 89,
+  },
+  {
+    name: "React Js",
+    icon: <SiReact />,
+    percentage: 92,
+  },
+  {
+    name: "Next Js",
+    icon: <SiNextdotjs />,
+    percentage: 90,
+  },
+  {
+    name: "Node Js",
+    icon: <SiNodedotjs />,
+    percentage: 88,
+  },
+  {
+    name: ".NET",
+    icon: <SiDotnet />,
+    percentage: 80,
+  },
+  {
+    name: "C#",
+    icon: <SiSharp />,
+    percentage: 82,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: <SiTailwindcss />,
+    percentage: 94,
+  },
+  {
+    name: "Typescript",
+    icon: <SiTypescript />,
+    percentage: 90,
+  },
 ];
 
 const Skills = () => {
   return (
     <div className="text-white pt-16 pb-16">
-        <h1 className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white">
-           My <span className="text-cyan-300">Skills</span>
-        </h1>
-        <div className="flex flex-wrap justify-center gap-6 mt-16">
-            {skills.map((skil, i) => {
-                return <Tilt key={skil.name} scale={1.5} transitionSpeed={400}>
-                   <div data-aos="flip-right" data-aos-anchor-placement="top-center" data-aos-delay={i * 100} className="bg-[#14134145] text-center w-40 h-48 rounded-3xl flex flex-col items-center justify-center shadow-lg transition hover:scale-105">
-                    <div className="text-5xl mb-4 text-gray-300">{skil.icon}</div>
-                    <p className="text-2xl font-semibold">{skil.percentage}%</p>
-                    <p className="text-purple-400 mt-1">{skil.name}</p>
-                    </div> 
-                </Tilt>
-            })}
-        </div>
-        </div>
-  )
-}
+      <h1 className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white">
+        My <span className="text-cyan-300">Skills</span>
+      </h1>
+      <div className="flex flex-wrap justify-center gap-6 mt-16">
+        {skills.map((skil, i) => {
+          return (
+            <Tilt key={skil.name} scale={1.5} transitionSpeed={400}>
+              <div
+                data-aos="flip-right"
+                data-aos-anchor-placement="top-center"
+                data-aos-delay={i * 100}
+                className="bg-[#14134145] text-center w-40 h-48 rounded-3xl flex flex-col items-center justify-center shadow-lg transition hover:scale-105"
+              >
+                <div className="text-5xl mb-4 text-gray-300">{skil.icon}</div>
+                <p className="text-2xl font-semibold">{skil.percentage}%</p>
+                <p className="text-purple-400 mt-1">{skil.name}</p>
+              </div>
+            </Tilt>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Skills
+export default Skills;
